a364c3758f003aab1485f355387ddaf4de4ca28d
khs05109 <65418964+khs05109@users.noreply.github.com>
2023-09-09 17:56:07 +0900

    sns_1

---
+++ sns/sns/src/main/java/com/example/sns/TestController.java
@@ -0,0 +1,16 @@
+package com.example.sns;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1")
+public class TestController {
+    @GetMapping("/hello")
+    public ResponseEntity<String> hello(@RequestParam String name) {
+        return ResponseEntity.ok("hello" + name);
+    }
+}
